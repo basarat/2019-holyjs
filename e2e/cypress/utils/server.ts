c1ada5bf1ae7619fc8f58ce22b1c69ee7e907f5d
Basarat <basaratali@gmail.com>
2019-04-26 16:06:45 +1000

    mention failure

--- e2e/cypress/utils/server.ts
+++ e2e/cypress/utils/server.ts
@@ -2,8 +2,16 @@ export function startServer() {
   let startId = 0;
   cy.server()
     .route('GET', '/api/get-all', { todos: [] })
-    .route('POST', '/api/add', (_req: any) => {
-      return { id: (startId++).toString() }
+    /** Dynamic fail https://github.com/cypress-io/cypress/issues/521  */
+    .route({
+      method: 'POST',
+      url: '/api/add',
+      response: { id: '' },
+      onResponse(xhr: XMLHttpRequest) {
+        xhr.response.body = {
+          id: (++startId).toString()
+        }
+      }
     })
     .route('PUT', '/api/set-all', {})
 }
