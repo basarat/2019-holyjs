287090c24a8ea310344eba3f80f1ce1b6aef67ab
Basarat <basaratali@gmail.com>
2019-05-10 13:17:54 +1000

    clear and persistence left

--- e2e/cypress/integration/example.ts
+++ e2e/cypress/integration/example.ts
@@ -148,14 +148,9 @@ describe('todo mvc', () => {
 `
 
 ### Clear completed button
-
 Removes completed todos when clicked. Should be hidden when there are no completed todos.
 
-### Persistence
-
-Your app should dynamically persist the todos to localStorage. If the framework has capabilities for persisting data (e.g. Backbone.sync), use that. Otherwise, use vanilla localStorage. If possible, use the keys 'id', 'title', 'completed' for each item. Make sure to use this format for the localStorage name: 'todos-[framework]'. Editing mode should not be persisted.
 
 ### Routing
-
 Routing is required for all implementations. If supported by the framework, use its built-in capabilities. Otherwise, use the  [Flatiron Director](https://github.com/flatiron/director) routing library located in the '/assets' folder. The following routes should be implemented: '#/' (all - default), '#/active' and '#/completed' ('#!/' is also allowed). When the route changes, the todo list should be filtered on a model level and the 'selected' class on the filter links should be toggled. When an item is updated while in a filtered state, it should be updated accordingly. E.g. if the filter is 'Active' and the item is checked, it should be hidden. Make sure the active filter is persisted on reload.
 `
