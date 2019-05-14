141228568a1cc183bb4ba3a9a0cfcd9723869bfd
Basarat <basaratali@gmail.com>
2019-05-14 16:18:00 +1000

    practiced

--- e2e/cypress/integration/todoPage/020 Item.ts
+++ e2e/cypress/integration/todoPage/020 Item.ts
@@ -17,19 +17,12 @@ beforeEach(() => {
 
 describe('Item', () => {
   it('Starts of unchecked', () => {
-    page.addTodo('Hello World');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).should('not.be.checked');
+    
   });
   it('Clicking the checkbox toggles the todo active/complete', () => {
-    page.addTodo('Hello World');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).should('be.checked');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).should('not.be.checked');
+    
   });
   it('Clicking the remove button should remove it item', () => {
-    page.addTodo('Hello world');
-    cy.get(page.selectors.itemDestroyByIndex(0)).invoke('show').click();
-    cy.get('Hello World').should('not.exist');
+    
   });
 });
