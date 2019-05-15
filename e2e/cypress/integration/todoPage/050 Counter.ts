0f82455b6edac9c7e40cedd26edd91bfc6ccc9d8
Basarat <basaratali@gmail.com>
2019-05-15 12:24:12 +1000

    prep

--- e2e/cypress/integration/todoPage/050 Counter.ts
+++ e2e/cypress/integration/todoPage/050 Counter.ts
@@ -15,16 +15,9 @@ beforeEach(() => {
 `
 describe('Counter', () => {
   it('Is not displayed when there are no items', () => {
-    cy.get(page.selectors.todoCount).should('not.exist');
+    
   });
   it('Displays the number of active todos in a pluralized form e.g. "0 items left", "1 item left", "2 items left"', () => {
-    page.addTodo('Hello World');
-    cy.get(page.selectors.todoCount).should('have.text', '1 item left');
-
-    page.addTodo('Again');
-    cy.get(page.selectors.todoCount).should('have.text', '2 items left');
-
-    cy.get(page.selectors.toggleAllCheckbox).click();
-    cy.get(page.selectors.todoCount).should('have.text', '0 items left');
+    
   });
 });
