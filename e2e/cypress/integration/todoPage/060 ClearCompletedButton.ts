ecf4b24d39b47c7c84861cd92438167450dce851
Basarat <basaratali@gmail.com>
2019-05-15 12:28:18 +1000

    prep

--- e2e/cypress/integration/todoPage/060 ClearCompletedButton.ts
+++ e2e/cypress/integration/todoPage/060 ClearCompletedButton.ts
@@ -16,18 +16,12 @@ beforeEach(() => {
 `
 describe('Clear completed button', () => {
   it('Should be hidden when there are no completed todos', () => {
-    page.addTodo('Hello');
-    cy.get(page.selectors.clearCompleted).should('not.exist');
+    
   });
   it('Should be visible when there are no completed todos', () => {
-    page.addTodo('Hello');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.clearCompleted).should('exist');
+    
   });
   it('Clicking it removes completed todos', () => {
-    page.addTodo('Hello');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.clearCompleted).click();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('not.exist');
+    
   });
 });
