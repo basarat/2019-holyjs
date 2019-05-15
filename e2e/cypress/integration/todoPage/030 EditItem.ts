a2d304e905aca6e59848d9bcdcf4da15acacc754
Basarat <basaratali@gmail.com>
2019-05-15 11:59:25 +1000

    prepping

--- e2e/cypress/integration/todoPage/030 EditItem.ts
+++ e2e/cypress/integration/todoPage/030 EditItem.ts
@@ -23,42 +23,24 @@ describe('Edit item', () => {
     page.addTodo('Hello');
   });
   it('Double-clicking the todo label activates editing mode', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).should('exist');
+    
   });
   it('The edit mode should exit on enter, blur and escape', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type('{enter}').should('not.exist');
-
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).blur().should('not.exist');
-
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type('{esc}').should('not.exist');
+    
   });
   it('Enter results in a commit', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type(' World{enter}');
-    cy.get(page.selectors.itemLabelByIndex(0)).should('have.text', 'Hello World');
+    
   });
   it('Blur results in a commit', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type(' World').blur();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('have.text', 'Hello World');
+    
   });
   it('The *commit* is done after trim', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type(' World ').blur();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('have.text', 'Hello World');
+    
   });
   it('If the trim results in an empty value, the commit should destroy the item', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).clear().blur();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('not.exist');
+    
   });
   it('Escape does not result in a commit', () => {
-    cy.get(page.selectors.itemLabelByIndex(0)).dblclick();
-    cy.get(page.selectors.itemEditByIndex(0)).type(' World{esc}');
-    cy.get(page.selectors.itemLabelByIndex(0)).should('have.text', 'Hello');
+    
   });
 });
