79130552926d58e75d1ec17d06fcd9b93b3e41a0
Basarat <basaratali@gmail.com>
2019-05-15 10:03:38 +1000

    cleaner start

--- e2e/cypress/integration/example.ts
+++ e2e/cypress/integration/example.ts
@@ -1,19 +1,7 @@
 /// <reference types="cypress"/>
 
-import { startServer } from "../utils/server";
-import * as page from "../utils/pages/todoPage";
-
-beforeEach(() => {
-  startServer();
-  page.visit();
-});
-
 describe('Example', () => {
   it('Should work', () => {
-    page.addTodo('one');
-    page.addTodo('two');
 
-    cy.contains('one');
-    cy.contains('two');
   });
 });
