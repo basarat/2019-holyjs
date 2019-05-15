061e8bd3cda5521d5450237f89e200598df9cdab
Basarat <basaratali@gmail.com>
2019-05-15 12:34:59 +1000

    preping

--- e2e/cypress/integration/todoPage/070 Routing.ts
+++ e2e/cypress/integration/todoPage/070 Routing.ts
@@ -27,27 +27,18 @@ describe('Routing', () => {
     page.addTodo('InProgress');
   });
   it('"#/" (default) - all items are shown. The all link is selected', () => {
-    cy.get(page.selectors.all).should('have.class', page.classNames.selectedFilter);
-    page.getAllTodos().should('deep.equal', ['Completed', 'InProgress']);
+    
   });
   it('"#/active" - Only active items are shown. The active link is selected', () => {
-    cy.visit('#/active');
-    cy.get(page.selectors.active).should('have.class', page.classNames.selectedFilter);
-    page.getAllTodos().should('deep.equal', ['InProgress']);
+    
   });
   it('"#/completed" - Only completed items are shown. The completed link is selected', () => {
-    cy.visit('#/completed');
-    cy.get(page.selectors.completed).should('have.class', page.classNames.selectedFilter);
-    page.getAllTodos().should('deep.equal', ['Completed']);
+    
   });
   it('"#/active" - Items should move out if checked', () => {
-    cy.visit('#/active');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('not.exist');
+    
   });
   it('"#/completed" - Items should move out if unchecked', () => {
-    cy.visit('#/completed');
-    cy.get(page.selectors.itemCheckBoxByIndex(0)).click();
-    cy.get(page.selectors.itemLabelByIndex(0)).should('not.exist');
+    
   });
 });
